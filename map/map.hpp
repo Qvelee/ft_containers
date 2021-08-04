52f210000f46fd44779c4479998d3e4fb235c258
nelisabe <aleksandr.engoyan@gmail.com>
2021-08-04 13:58:43 +0300

    add map class, implemented pair class

---
+++ map/map.hpp
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nelisabe <nelisabe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/04 12:43:56 by nelisabe          #+#    #+#             */
+/*   Updated: 2021/08/04 13:58:01 by nelisabe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAP_HPP
+# define MAP_HPP
+
+# include <functional>
+# include <memory>
+# include <cstddef>
+# include <cstdlib>
+# include "pair.hpp"
+
+namespace ft
+{
+
+template<typename key, typename T, typename Compare = std::less<key>,
+	typename Allocator = std::allocator<pair<const key, T>>>
+class map
+{
+	public:
+		typedef key									key_type;
+		typedef T									mapped_type;
+		typedef	pair<const key_type, mapped_type>	value_type;
+		typedef Compare								key_compare;
+		typedef	Compare								value_compare; // ????
+		typedef Allocator							allocator_type;
+		typedef value_type&							reference;
+		typedef const value_type&					const_reference;
+		typedef value_type*							pointer;
+		typedef const value_type*					const_pointer;
+		typedef iterator; // TODO
+		typedef const_iterator;
+		typedef reverse_iterator;
+		typedef const_reverse_iterator;
+		typedef std::ptrdiff_t						difference_type;
+		typedef	size_t								size_type;
+
+		explicit map(const key_compare &compare = key_compare(),
+			const allocator_type &allocator = allocator_type()); // empty
+		template<typename InputIterator>
+		map(InputIterator first, InputIterator last,
+			const key_compare &compare = key_compare(),
+			const allocator_type &allocator = allocator_type()); // range
+		map(const map &source); // copy
+		~map();
+
+		map			&operator=(const map &source);
+		mapped_type &operator[](const key_type &key_);
+
+		iterator				begin();
+		const_iterator			begin();
+		iterator				end();
+		const_iterator			end();
+		reverse_iterator		rbegin();
+		const_reverse_iterator	rbegin() const;
+		reverse_iterator		rend();
+		const_reverse_iterator	rend() const;
+		bool					empty() const;
+		size_type				size() const;
+		size_type				max_size() const;
+		pair<iterator, bool>	insert(const value_type &value);
+		iterator				insert(iterator position, const value_type &value);
+		template<typename InputIterator>
+		void					insert(InputIterator first, InputIterator last);
+		void					erase(iterator position);
+		size_type				erase(const key_type &key_);
+		void					erase(iterator first, iterator last);
+		void					swap(map &source);
+		void					clear();
+		key_compare				key_comp() const;
+		value_compare			value_comp() const;
+		iterator				find(const key_type &key_);
+		const_iterator			find(const key_type &key_) const;
+		size_type				count(const key_type &key_) const;
+		iterator				lower_bound(const key_type &key_);
+		const_iterator			lower_bound(const key_type &key_) const;
+		iterator				upper_bound(const key_type &key_);
+		const_iterator			upper_bound(const key_type &key_) const;
+		pair<iterator, iterator>
+								equal_range(const key_type &key_) const;
+		pair<const_iterator, const_iterator>
+								equal_range(const key_type &key_) const;
+		allocator_type			get_allocator() const;
+	private:
+	
+};
+
+
+} // namespace ft
+#endif
---
+++ map/pair.hpp
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pair.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nelisabe <nelisabe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/04 12:51:23 by nelisabe          #+#    #+#             */
+/*   Updated: 2021/08/04 13:39:03 by nelisabe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PAIR_HPP
+# define PAIR_HPP
+
+# include "utils.hpp"
+
+namespace ft
+{
+
+template<typename T1, typename T2>
+struct pair
+{
+	typedef T1	first_type;
+	typedef T2	second_type;
+	
+	pair(); // default
+	template<typename U, typename V>
+	pair(const pair<U, V> &source); // copy
+	pair(const first_type &fvalue, const second_type &svalue); // initialization
+	~pair();
+
+	pair	&operator=(const pair &source);
+
+	T1	first;
+	T2	second;
+};
+
+template<typename T1, typename T2>
+pair<T1, T2>::
+pair()
+{
+	first = T1();
+	second = T2();
+}
+
+template<typename T1, typename T2>
+template<typename U, typename V>
+pair<T1, T2>::
+pair(const pair<U, V> &source)
+{
+	*this = source;
+}
+
+template<typename T1, typename T2>
+pair<T1, T2>::
+pair(const first_type &fvalue, const second_type &svalue)
+{
+	first(fvalue);
+	second(svalue);
+}
+
+template<typename T1, typename T2>
+pair<T1, T2>::
+~pair() { }
+
+template<typename T1, typename T2>
+pair<T1, T2>	&pair<T1, T2>::
+operator=(const pair &source)
+{
+	first = source.first;
+	second = source.second;
+	return *this;
+}
+
+template<typename T1, typename T2>
+bool operator==(const pair<T1, T2> &left, const pair<T1, T2> &rigth);
+
+template<typename T1, typename T2>
+bool operator!=(const pair<T1, T2> &left, const pair<T1, T2> &rigth);
+
+template<typename T1, typename T2>
+bool operator< (const pair<T1, T2> &left, const pair<T1, T2> &rigth);
+
+template<typename T1, typename T2>
+bool operator<=(const pair<T1, T2> &left, const pair<T1, T2> &rigth);
+
+template<typename T1, typename T2>
+bool operator> (const pair<T1, T2> &left, const pair<T1, T2> &rigth);
+
+template<typename T1, typename T2>
+bool operator>=(const pair<T1, T2> &left, const pair<T1, T2> &rigth);
+
+} // namespace ft
+
+#endif
