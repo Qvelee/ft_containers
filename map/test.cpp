c9f90a022a222e43d26f9fe0ba09627d0d6031d2
nelisabe <aleksandr.engoyan@gmail.com>
2021-08-10 14:54:24 +0300

    deleting nodes with only one child

--- map/test.cpp
+++ map/test.cpp
@@ -6,7 +6,7 @@
 /*   By: nelisabe <nelisabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 13:13:04 by nelisabe          #+#    #+#             */
-/*   Updated: 2021/08/10 13:51:43 by nelisabe         ###   ########.fr       */
+/*   Updated: 2021/08/10 14:52:29 by nelisabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@ using namespace ft;
 int		main()
 {
 	Tree<int, void, void>	tree;
+	int		array[] = {10, 7, 3, 8, 1, 4, 20, 15, 25, 13, 17, 16, 30, 29, 35 };
 
-	tree.Add(5);
-	tree.Add(5);
-	tree.Add(6);
-	tree.Add(3);
+	for (int i = 0; i < sizeof(array) / sizeof(array[0]); ++i)
+		tree.Add(array[i]);
 	tree.Print();
+	// std::cout << "----------\n";
+	// tree.Print();
 	return 0;
 }
--- map/tree.hpp
+++ map/tree.hpp
@@ -6,7 +6,7 @@
 /*   By: nelisabe <nelisabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 12:20:00 by nelisabe          #+#    #+#             */
-/*   Updated: 2021/08/10 14:02:20 by nelisabe         ###   ########.fr       */
+/*   Updated: 2021/08/10 14:53:42 by nelisabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,12 @@ namespace ft
 template<typename T>
 struct Node
 {
-	Node(const T &data_, Node *parent_, Node *left_, Node *rigth_)
+	Node(const T &data_, Node *parent_)
 	{
 		data = data_;
 		parent = parent_;
-		left = left_;
-		rigth = rigth_;
+		left = NULL;
+		rigth = NULL;
 	}
 	~Node() { }
 
@@ -63,7 +63,10 @@ class Tree
 		void			Print();
 	private:
 		void			_Print(Node<data_type> *node);
-		Node<data_type>	*_Add(Node<data_type> *node, const data_type &data);
+		Node<data_type>	*_Add(Node<data_type> *node, Node<data_type> *parent,
+			const data_type &data);
+		Node<data_type>	*_Delete(Node<data_type> *node, Node<data_type> *parent,
+			const data_type &data);
 		void			_Destroy(Node<data_type> *node);
 
 		size_type		_size;
@@ -122,23 +125,23 @@ template<typename Data, typename Key_from_data,
 void	Tree<Data, Key_from_data, Compare, Allocator>::
 Add(const data_type &data)
 {
-	_tree = _Add(_tree, data);
+	_tree = _Add(_tree, NULL, data);
 }
 
 template<typename Data, typename Key_from_data,
 	typename Compare, typename Allocator>
 Node<Data>	*Tree<Data, Key_from_data, Compare, Allocator>::
-_Add(Node<data_type> *node, const data_type &data)
+_Add(Node<data_type> *node, Node<data_type> *parent, const data_type &data)
 {
 	if (node == NULL)
 	{
-		node = new Node<data_type>(data, NULL, NULL, NULL);
+		node = new Node<data_type>(data, parent);
 		return node;
 	}
 	if (data < node->data) // TODO add compare function
-		node->left = _Add(node->left, data); 
+		node->left = _Add(node->left, node, data); 
 	if (data > node->data) // TODO add compare function
-		node->rigth = _Add(node->rigth, data); 
+		node->rigth = _Add(node->rigth, node, data);
 	return node;
 }
 
@@ -147,7 +150,30 @@ template<typename Data, typename Key_from_data,
 void	Tree<Data, Key_from_data, Compare, Allocator>::
 Delete(const data_type &data)
 {
-	// TODO
+	_tree = _Delete(_tree, NULL, data);
+}
+
+template<typename Data, typename Key_from_data,
+	typename Compare, typename Allocator>
+Node<Data>	*Tree<Data, Key_from_data, Compare, Allocator>::
+_Delete(Node<data_type> *node, Node<data_type> *parent, const data_type &data)
+{
+	if (node == NULL)
+		return node;
+	if (data < node->data)
+		node->left = _Delete(node->left, node, data);
+	else if (data > node->data)
+		node->rigth = _Delete(node->rigth, node, data);
+	else if (node->left == NULL || node->rigth == NULL)
+	{
+		Node<data_type>	*temp;
+		
+		temp = node->left ? node->left : node->rigth;
+		temp->parent = parent;
+		delete node;
+		return temp;
+	}
+	return node;
 }
 
 template<typename Data, typename Key_from_data,
@@ -174,7 +200,12 @@ _Print(Node<data_type> *node)
 	if (node == NULL)
 		return;
 	_Print(node->left);
-	std::cout << node->data << std::endl;
+	std::cout << "Data: " << node->data << ' ';
+	std::cout << "Parent: ";
+	if (node->parent)
+		std::cout << node->parent->data << std::endl;
+	else
+		std::cout << "root" << std::endl;
 	_Print(node->rigth);
 }
 
