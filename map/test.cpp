d79667ab836791642cb2382ad32dfea53171411d
nelisabe <aleksandr.engoyan@gmail.com>
2021-08-10 17:38:30 +0300

    delete any elements, print free by width

--- map/test.cpp
+++ map/test.cpp
@@ -6,7 +6,7 @@
 /*   By: nelisabe <nelisabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 13:13:04 by nelisabe          #+#    #+#             */
-/*   Updated: 2021/08/10 14:52:29 by nelisabe         ###   ########.fr       */
+/*   Updated: 2021/08/10 17:37:49 by nelisabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,20 @@ using namespace ft;
 int		main()
 {
 	Tree<int, void, void>	tree;
-	int		array[] = {10, 7, 3, 8, 1, 4, 20, 15, 25, 13, 17, 16, 30, 29, 35 };
+	int		array[] = { 10, 7, 3, 8, 1, 5, 4, 6, 20, 15, 25, 13, 17, 16, 30, 29, 35 };
 
 	for (int i = 0; i < sizeof(array) / sizeof(array[0]); ++i)
 		tree.Add(array[i]);
+	tree.PrintWidth();
+	std::cout << "----------\n";
+	tree.Print();
+	std::cout << "----------\n";
+	tree.Delete(20);
 	tree.Print();
 	// std::cout << "----------\n";
+	// tree.Delete(7);
 	// tree.Print();
+	std::cout << "----------\n";
+	tree.PrintWidth();
 	return 0;
 }
--- map/tree.hpp
+++ map/tree.hpp
@@ -6,7 +6,7 @@
 /*   By: nelisabe <nelisabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 12:20:00 by nelisabe          #+#    #+#             */
-/*   Updated: 2021/08/10 14:53:42 by nelisabe         ###   ########.fr       */
+/*   Updated: 2021/08/10 17:37:39 by nelisabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <cstdlib>
 # include <functional>
 # include <memory>
+# include <queue>
 # include <iostream> // DELETE
 
 namespace ft
@@ -61,8 +62,10 @@ class Tree
 		void			Delete(const data_type &data);
 		Node<data_type>	*Find(const data_type &data);
 		void			Print();
+		void			PrintWidth();
 	private:
-		void			_Print(Node<data_type> *node);
+		void			Print(Node<data_type> *node);
+		void			PrintWidth(Node<data_type> *node);
 		Node<data_type>	*_Add(Node<data_type> *node, Node<data_type> *parent,
 			const data_type &data);
 		Node<data_type>	*_Delete(Node<data_type> *node, Node<data_type> *parent,
@@ -150,7 +153,7 @@ template<typename Data, typename Key_from_data,
 void	Tree<Data, Key_from_data, Compare, Allocator>::
 Delete(const data_type &data)
 {
-	_tree = _Delete(_tree, NULL, data);
+	_tree = _Delete(_tree, NULL, data); // TODO add rigth delete (allocator)
 }
 
 template<typename Data, typename Key_from_data,
@@ -169,9 +172,32 @@ _Delete(Node<data_type> *node, Node<data_type> *parent, const data_type &data)
 		Node<data_type>	*temp;
 		
 		temp = node->left ? node->left : node->rigth;
-		temp->parent = parent;
+		if (temp != NULL)
+			temp->parent = parent;
 		delete node;
 		return temp;
+	}
+	else
+	{
+		Node<data_type>	*child = node->rigth;
+
+		for (; child->left != NULL; child = child->left);
+		if (child->parent != node)
+			child->parent->left = child->rigth;
+		child->left = node->left;
+		if (child->parent != node)
+			child->rigth = node->rigth;
+		child->parent = node->parent;
+		if (child->left != NULL)
+			child->left->parent = child;
+		if (child->rigth != NULL)
+			child->rigth->parent = child;
+		if (node->parent->rigth == node)
+			node->parent->rigth = child;
+		else
+			node->parent->left = child;
+		delete node;
+		return child;
 	}
 	return node;
 }
@@ -181,7 +207,7 @@ template<typename Data, typename Key_from_data,
 Node<Data>	*Tree<Data, Key_from_data, Compare, Allocator>::
 Find(const data_type &data)
 {
-	
+	// TODO
 }
 
 template<typename Data, typename Key_from_data,
@@ -189,24 +215,59 @@ template<typename Data, typename Key_from_data,
 void	Tree<Data, Key_from_data, Compare, Allocator>::
 Print()
 {
-	_Print(_tree);
+	Print(_tree);
 }
 	
 template<typename Data, typename Key_from_data,
 	typename Compare, typename Allocator>
 void	Tree<Data, Key_from_data, Compare, Allocator>::
-_Print(Node<data_type> *node)
+Print(Node<data_type> *node)
 {
 	if (node == NULL)
 		return;
-	_Print(node->left);
+	Print(node->left);
 	std::cout << "Data: " << node->data << ' ';
 	std::cout << "Parent: ";
 	if (node->parent)
 		std::cout << node->parent->data << std::endl;
 	else
 		std::cout << "root" << std::endl;
-	_Print(node->rigth);
+	Print(node->rigth);
+}
+
+template<typename Data, typename Key_from_data,
+	typename Compare, typename Allocator>
+void	Tree<Data, Key_from_data, Compare, Allocator>::
+PrintWidth()
+{
+	PrintWidth(_tree);
+}
+
+template<typename Data, typename Key_from_data,
+	typename Compare, typename Allocator>
+void	Tree<Data, Key_from_data, Compare, Allocator>::
+PrintWidth(Node<data_type> *node)
+{
+	if (node == NULL)
+		return;
+	std::queue<Node<data_type>*>	queue;
+	size_t							size;
+
+	queue.push(node);
+	while (!queue.empty())
+	{
+		size = queue.size();
+		for (size_t i = 0; i < size; ++i)
+		{
+			std::cout << queue.front()->data << ' ';
+			if (queue.front()->left)
+				queue.push(queue.front()->left);
+			if (queue.front()->rigth)
+				queue.push(queue.front()->rigth);
+			queue.pop();
+		}
+		std::cout << std::endl;
+	}
 }
 
 } // namespace ft
