c18afbdf23e5605c0918097d2ec373089195c724
nelisabe <aleksandr.engoyan@gmail.com>
2021-08-12 15:18:20 +0300

    test file for tree.hpp

---
+++ map/old_test.cpp
@@ -0,0 +1,140 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nelisabe <nelisabe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/10 13:13:04 by nelisabe          #+#    #+#             */
+/*   Updated: 2021/08/12 13:17:05 by nelisabe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "tree.hpp"
+# include <functional>
+# include <map>
+
+using namespace ft;
+
+template<typename Pair>
+struct SelectFirst
+{
+	typename Pair::first_type &operator()(Pair &pair) const
+	{
+		return pair.first;
+	}
+	const typename Pair::first_type &operator()(const Pair &pair) const
+	{
+		return pair.first;
+	}
+};
+
+template<typename T>
+struct Select
+{
+	T &operator()(T &x) const
+	{
+		return x;
+	}
+	const T &operator()(const T &x) const
+	{
+		return x;
+	}
+};
+
+int		main()
+{
+	Tree<int, Select<int>, std::less<int> >	tree;
+	int		array[] = { 10, 7, 3, 8, 1, 5, 4, 6, 20, 15, 25, 13, 17, 16, 30, 29, 35 };
+
+	std::cout << "Size: " << tree.Size() << std::endl;
+	for (int i = 0; i < sizeof(array) / sizeof(array[0]); ++i)
+		tree.Add(array[i]);
+	std::cout << "----------\n";
+	tree.Print();
+	// std::cout << "Size: " << tree.Size() << std::endl;
+	// std::cout << "----------\n";
+	// tree.Delete(10);
+	// tree.Print();
+	// std::cout << "Size: " << tree.Size() << std::endl;
+	// std::cout << "----------\n";
+	// tree.PrintWidth();
+	// std::cout << "Data found: " << tree.Find(6)->data << std::endl;
+	// std::cout << "Data found: ";
+	// if (tree.Find(99) == NULL)
+	// 	std::cout << "no\n";
+	
+
+	// Tree<int, Select<int>, std::less<int> >	tree2;
+	// tree2.Add(4);
+	// tree2.Delete(4);
+	// tree2.Delete(5);
+	// std::cout << "Size: " << tree2.Size() << std::endl;
+	// tree.Delete(20);
+	// tree.Print();
+	// // std::cout << "----------\n";
+	// // tree.Delete(7);
+	// // tree.Print();
+	// std::cout << "----------\n";
+	// tree.PrintWidth();
+	// tree.PrintMinMax();
+
+
+	// for (auto it = tree.begin(); it != tree.end(); ++it)
+	// {
+		// std::cout << *it << std::endl;
+	// }
+	// auto it = tree.begin();
+	// std::cout << *it++ << std::endl;
+	// std::cout << *it << std::endl;
+
+	// auto it = tree.end();
+	// --it;
+	// std::cout << *it << std::endl;
+	// for (auto it = --tree.end(); it != tree.begin(); --it)
+	// {
+	// 	std::cout << *it << std::endl;
+	// }
+	// auto it = --tree.end();
+	// std::cout << *it-- << std::endl;
+	// std::cout << *it << std::endl;
+	// it++;
+	// std::cout << *it << std::endl;
+	
+	// Tree<int, Select<int>, std::less<int> >	tree2;
+	// tree2.Add(4);
+	// tree2.Add(5);
+	// tree2.Add(6);
+// 
+	// tree = tree2;
+	// for (auto it = tree.begin(); it != tree.end(); ++it)
+	// {
+		// std::cout << *it << std::endl;
+	// }
+	
+	for (Tree<int, Select<int>, std::less<int> >::iterator it =
+		tree.begin(); it != tree.end(); ++it)
+	{
+		std::cout << *it << std::endl;
+	}
+	std::cout << "-----------\n";
+	for (Tree<int, Select<int>, std::less<int> >::const_iterator it =
+		tree.begin(); it != tree.end(); ++it)
+	{
+		std::cout << *it << std::endl;
+	}
+	std::cout << "-----------\n";
+	for (Tree<int, Select<int>, std::less<int> >::reverse_iterator it =
+		tree.rbegin(); it != tree.rend(); ++it)
+	{
+		std::cout << *it << std::endl;
+	}
+	std::cout << "-----------\n";
+	for (Tree<int, Select<int>, std::less<int> >::const_reverse_iterator it =
+		tree.rbegin(); it != tree.rend(); ++it)
+	{
+		std::cout << *it << std::endl;
+	}
+
+	return 0;
+}
